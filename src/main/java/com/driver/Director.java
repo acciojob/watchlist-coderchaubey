242ad82b099f2720002c99169128512a333afc0f
coderchaubey <sunnychaubeysm@gmail.com>
2023-01-29 12:59:15 +0530

    ab dekho

--- src/main/java/com/driver/Director.java
+++ src/main/java/com/driver/Director.java
@@ -1,24 +1,61 @@
 package com.driver;
 
-//String name, int durationInMinutes, double imdbRating,
-// no-args constructor, all-args constructor and getters-setters
+////String name, int durationInMinutes, double imdbRating,
+//// no-args constructor, all-args constructor and getters-setters
+//public class Director {
+//    private String name;
+//    private int durationInMinutes;
+//    private double imdbRating;
+//
+//    //default constructor
+//    public Director() {
+//    }
+//
+//    //parameterized constructor
+//    public Director(String name, int durationInMinutes, double imdbRating) {
+//        this.name = name;
+//        this.durationInMinutes = durationInMinutes;
+//        this.imdbRating = imdbRating;
+//    }
+//
+//    //getters and setters
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getDurationInMinutes() {
+//        return durationInMinutes;
+//    }
+//
+//    public void setDurationInMinutes(int durationInMinutes) {
+//        this.durationInMinutes = durationInMinutes;
+//    }
+//
+//    public double getImdbRating() {
+//        return imdbRating;
+//    }
+//
+//    public void setImdbRating(double imdbRating) {
+//        this.imdbRating = imdbRating;
+//    }
+//}
+
 public class Director {
+
     private String name;
-    private int durationInMinutes;
+    private int numberOfMovies;
     private double imdbRating;
 
-    //default constructor
-    public Director() {
-    }
-
-    //parameterized constructor
-    public Director(String name, int durationInMinutes, double imdbRating) {
+    public Director(String name, int numberOfMovies, double imdbRating) {
         this.name = name;
-        this.durationInMinutes = durationInMinutes;
+        this.numberOfMovies = numberOfMovies;
         this.imdbRating = imdbRating;
     }
 
-    //getters and setters
     public String getName() {
         return name;
     }
@@ -27,12 +64,12 @@ public class Director {
         this.name = name;
     }
 
-    public int getDurationInMinutes() {
-        return durationInMinutes;
+    public int getNumberOfMovies() {
+        return numberOfMovies;
     }
 
-    public void setDurationInMinutes(int durationInMinutes) {
-        this.durationInMinutes = durationInMinutes;
+    public void setNumberOfMovies(int numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
     }
 
     public double getImdbRating() {
@@ -42,4 +79,5 @@ public class Director {
     public void setImdbRating(double imdbRating) {
         this.imdbRating = imdbRating;
     }
+    //String name, int numberOfMovies, double imdbRating, no-args constructor, all-args constructor and getters-setters
 }
--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -1,23 +1,61 @@
 package com.driver;
 
-// String name, int numberOfMovies, double imdbRating,
-// no-args constructor, all-args constructor and getters-setters
+//// String name, int numberOfMovies, double imdbRating,
+//// no-args constructor, all-args constructor and getters-setters
+//public class Movie {
+//    private String name;
+//    private int numberOfMovies;
+//    private double imdbRating;
+//
+//    //default constructor
+//    public Movie() {
+//    }
+//
+//    //parameterized constructor
+//    public Movie(String name, int numberOfMovies, double imdbRating) {
+//        this.name = name;
+//        this.numberOfMovies = numberOfMovies;
+//        this.imdbRating = imdbRating;
+//    }
+//    //getters and setters
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getNumberOfMovies() {
+//        return numberOfMovies;
+//    }
+//
+//    public void setNumberOfMovies(int numberOfMovies) {
+//        this.numberOfMovies = numberOfMovies;
+//    }
+//
+//    public double getImdbRating() {
+//        return imdbRating;
+//    }
+//
+//    public void setImdbRating(double imdbRating) {
+//        this.imdbRating = imdbRating;
+//    }
+//}
+
+
 public class Movie {
-    private String name;
-    private int numberOfMovies;
+
+    private  String name;
+    private int durationInMinutes;
     private double imdbRating;
 
-    //default constructor
-    public Movie() {
-    }
-
-    //parameterized constructor
-    public Movie(String name, int numberOfMovies, double imdbRating) {
+    public Movie(String name, int durationInMinutes, double imdbRating) {
         this.name = name;
-        this.numberOfMovies = numberOfMovies;
+        this.durationInMinutes = durationInMinutes;
         this.imdbRating = imdbRating;
     }
-    //getters and setters
+
     public String getName() {
         return name;
     }
@@ -26,12 +64,12 @@ public class Movie {
         this.name = name;
     }
 
-    public int getNumberOfMovies() {
-        return numberOfMovies;
+    public int getDurationInMinutes() {
+        return durationInMinutes;
     }
 
-    public void setNumberOfMovies(int numberOfMovies) {
-        this.numberOfMovies = numberOfMovies;
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
     }
 
     public double getImdbRating() {
@@ -41,4 +79,5 @@ public class Movie {
     public void setImdbRating(double imdbRating) {
         this.imdbRating = imdbRating;
     }
+
 }
